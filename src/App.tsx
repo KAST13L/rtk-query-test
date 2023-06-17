86c07869f5ac3d45865c95e7e231aed1545ce752
KAST13L <o4enbsmeshno@gmail.com>
2023-06-17 17:49:21 +0300

    feat: use rtk toolkit and query

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ export const App: FC = ({}) => {
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
+        <option value="4">4</option>
       </select>
       <h2>hello world</h2>
       <div>
